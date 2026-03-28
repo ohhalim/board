e3203285b2ccabb95ceac8550ada71063e6815b2
ohhalim <ohhalim777@gmail.com>
2026-03-28 21:31:44 +0900

    refactor: 게시글 API 경로를 /posts에서 /articles로 변경

--- src/main/java/com/halim/board/controller/ArticleController.java
+++ src/main/java/com/halim/board/controller/ArticleController.java
@@ -1,4 +1,4 @@
-package com.halim.board.controller;
+package com.halim.board.controller; // com.halim.board.controller 패키지에 속함을 선언합니다.
 
 import com.halim.board.dto.ArticleRequest;
 import com.halim.board.dto.ArticleResponse;
@@ -15,12 +15,12 @@ import org.springframework.web.bind.annotation.*;
 public class ArticleController {
     private final ArticleService articleService;
 
-    @PostMapping("/posts")
+    @PostMapping("/articles")
     public ResponseEntity<ArticleResponse> createArticle(@Valid @RequestBody ArticleRequest articleRequest) {
         return ResponseEntity.ok(articleService.createArticle(articleRequest));
     }
 
-    @GetMapping("/posts/{articleId}")
+    @GetMapping("/articles/{articleId}")
     public ResponseEntity<ArticleResponse> getArticleById(@PathVariable Long articleId) {
         return ResponseEntity.ok(articleService.getArticleById(articleId));
     }
